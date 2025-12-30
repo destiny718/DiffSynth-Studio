43c48553ab8c87576ffdbecb90fe293514d7161d
Tianqi Zhang <zhangtq21@outlook.com>
2025-12-30 15:03:05 +0800

    Add metadata

--- examples/wanvideo/model_training/train.py
+++ examples/wanvideo/model_training/train.py
@@ -1,4 +1,4 @@
-import torch, os, argparse, accelerate, warnings
+import torch, os, argparse, accelerate, warnings, sys
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
 if PROJECT_ROOT not in sys.path:
